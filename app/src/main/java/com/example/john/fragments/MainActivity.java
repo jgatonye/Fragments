8f44c48e21bc7b5d38d05ffaf40c8915e78e8d6a
john <gatonyejmaina@gmail.com>
2017-04-04 11:50:14 +0300

    Implemented radioGroup

---
+++ app/src/main/java/com/example/john/fragments/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.john.fragments;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.moodButton) Button mMoodButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        mMoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+        });
+
+
+
+    }
+
+
+}
+
